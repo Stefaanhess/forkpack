10dbb5e03a7c42bf6227249a80cdd1f53f21f1d1
stefaanhess <hessmann.stefaan@googlemail.com>
2023-10-25 16:27:17 +0200

    test

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ sys.path.insert(0, os.path.abspath('../src/schnetpack'))
 
 extensions = [
         'sphinx.ext.autodoc',
-        ' sphinx.ext.autosummary',
+        'sphinx.ext.autosummary',
         ]
 
 templates_path = ['_templates']
